fb92420c67db948f0a256ffde173fa6c65693ea7
Tomas Figura <tomas.figura@post.cz>
2021-11-18 23:40:38 +0100

    Method now returning only odborky

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,7 +22,18 @@ class HomeController extends AbstractController
      */
     public function odborky(Connection $connection): Response
     {
-		$sql = "SELECT * FROM program";
+		$sql = "SELECT p.program_id,
+		p.program_name,
+		p.program_photo,
+		p.program_info,
+		p.program_pozn,
+		vk.vekova_kat_name,
+		s.stupen_name
+		FROM `program` p
+		LEFT JOIN program_kat pk ON p.program_kat_id=pk.program_kat_id
+		LEFT JOIN vekova_kat vk ON p.vekova_kat_id=vk.vekova_kat_id
+		LEFT JOIN stupen s ON p.stupen_id=s.stupen_id
+		AND pk.program_kat_name = 'Odborky'";
         $zaznamy = $connection->fetchAllAssociative($sql);
 
 		$response = new Response();
