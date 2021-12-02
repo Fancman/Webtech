37c3ba4495abacb2b3d4a7c38cd8c07942bfe386
Tomas Figura <tomas.figura@post.cz>
2021-12-02 22:50:50 +0100

    Returning activites in progress with tasks

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -62,7 +62,7 @@ class HomeController extends AbstractController
 		if(isset($user_id)
 		&& isset($task_id)){
 			try {
-				$stmt = $connection->prepare('INSERT INTO new_task_progress (user_id, task_id) VALUES (:user_id, :task_id)');
+				$stmt = $connection->prepare("INSERT INTO new_task_progress (user_id, task_id, state) VALUES (:user_id, :task_id, 'rozpracovane')");
         		$stmt->execute(
 					[
 						'user_id' => $user_id,
@@ -94,6 +94,90 @@ class HomeController extends AbstractController
 				'new_record_id' => $created_id,
 				'activity_id' => ($task_record ? $task_record[0]['activity_id'] : null)
 			]));
+
+			$response->setStatusCode(Response::HTTP_OK);
+
+			return $response;
+		}
+
+		$response->setStatusCode(Response::HTTP_BAD_REQUEST);
+
+        $response->setContent(json_encode([
+			'msg' => 'Wrong request'
+		]));
+
+        return $response;
+	}
+
+	/**
+     * @Route("/api/active", name="active")
+     */
+    public function active(Connection $connection, Request $request): Response
+    {
+		$request_content = strval($request->getContent());
+    	$params = json_decode($request_content, true);
+
+		$user_id = $request->query->get('user_id');
+
+		$response = new Response();
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+		if($params){
+			$user_id = is_null($user_id) ? (array_key_exists("user_id", $params) ? $params["user_id"] : null) : $user_id;
+		}
+
+		if(isset($user_id)){
+			try {
+				$resultSet = $connection->executeQuery('select q1.*, na.name as age_activity_name from (SELECT a.* FROM new_task_progress p
+				left join new_task t ON p.task_id = t.id
+				left JOIN new_activity a ON t.activity_id = a.id
+				WHERE user_id = ?
+				group by a.id, a.name, a.img_url, a.age_category_id, a.level, a.activity_type) q1
+				left join new_age_category na on q1.age_category_id = na.id', [
+					$user_id
+				]);
+
+				$users_activities = $resultSet->fetchAllAssociative();
+
+			} catch (\Throwable $th) {
+				$response->setStatusCode(Response::HTTP_BAD_REQUEST);
+
+				$response->setContent(json_encode([
+					'msg' => 'Could not be inserted into DB.',
+					'error' => $th->getMessage()
+				]));
+
+				return $response;
+			}
+
+			foreach ($users_activities as $u => $ua) {
+				$resultSet = $connection->executeQuery("select nt2.*,
+				CASE
+					WHEN ntp1.state = 'splnene' THEN 'splnene'
+					WHEN ntp1.state = 'rozpracovane' THEN 'rozpracovane'
+					ELSE 'nesplnene'
+				END as task_state from (select distinct nt.activity_id from new_task_progress ntp
+				left join new_task nt on ntp.task_id=nt.id
+				where user_id = ?) q1
+				JOIN new_task nt2 ON q1.activity_id = nt2.activity_id
+				left join (select * from new_task_progress ntp0 where ntp0.user_id = ?) ntp1 on nt2.id = ntp1.task_id
+				where nt2.activity_id = ?
+				", [
+					$user_id,
+					$user_id,
+					$ua['id']
+				]);
+
+				$activity_tasks = $resultSet->fetchAllAssociative();
+
+				if($activity_tasks){
+					$users_activities[$u]['tasks'] = $activity_tasks;
+				}
+			}
+
+			$response->setContent(json_encode($users_activities));
 
 			$response->setStatusCode(Response::HTTP_OK);
 
