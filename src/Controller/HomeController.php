364936edaa08594876a72d5efbdce4a8c52780dd
Tomas Figura <tomas.figura@post.cz>
2021-12-05 15:43:59 +0100

    Activating tasks in activity

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -60,7 +60,7 @@ class HomeController extends AbstractController
 			$task_id = is_null($task_id) ? (array_key_exists("task_id", $params) ? $params["task_id"] : null) : $task_id;
 			$task_state = is_null($task_state) ? (array_key_exists("task_state", $params) ? $params["task_state"] : null) : $task_state;
 
-			if(!in_array($task_state, ['rozpracovane', 'splnene', 'nesplnene'])){
+			if($task_state && !in_array($task_state, ['rozpracovane', 'splnene', 'nesplnene'])){
 				$response->setStatusCode(Response::HTTP_BAD_REQUEST);
 
 				$response->setContent(json_encode([
@@ -235,6 +235,196 @@ class HomeController extends AbstractController
 			}
 
 			$response->setContent(json_encode($users_activities));
+
+			$response->setStatusCode(Response::HTTP_OK);
+
+			return $response;
+		}
+
+		$response->setStatusCode(Response::HTTP_BAD_REQUEST);
+
+        $response->setContent(json_encode([
+			'msg' => 'Wrong request'
+		]));
+
+        return $response;
+	}
+
+	/**
+     * @Route("/api/add-activity", name="add-activity")
+     */
+    public function add_activity(Connection $connection, Request $request): Response
+    {
+		$request_content = strval($request->getContent());
+    	$params = json_decode($request_content, true);
+
+		$user_id = $request->query->get('user_id');
+		$activity_id = $request->query->get('activity_id');
+
+		$response = new Response();
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+		if($params){
+			$user_id = is_null($user_id) ? (array_key_exists("user_id", $params) ? $params["user_id"] : null) : $user_id;
+			$activity_id = is_null($activity_id) ? (array_key_exists("activity_id", $params) ? $params["activity_id"] : null) : $activity_id;
+		}
+
+		$completed_activities = [];
+
+		if(isset($user_id) && isset($activity_id)){
+
+			$row_counts = 0;
+
+			$resultSet = $connection->executeQuery('SELECT * FROM new_task WHERE activity_id = ?', [
+				$activity_id
+			]);
+
+			$ulohy = $resultSet->fetchAllAssociative();
+
+			foreach ($ulohy as $u) {
+				try {
+					$stmt = $connection->prepare('INSERT INTO new_task_progress (user_id, task_id, state) VALUES (?, ?, ?)');
+
+					$execute = $stmt->execute([
+						$user_id,
+						$u['id'],
+						'rozpracovane'
+					]);
+	
+					$row_count = $execute->rowCount();
+					$row_counts += $row_count;
+				} catch (\Throwable $th) {
+					//echo $th->getMessage();
+				}
+			}
+
+			$response->setContent(json_encode(['message' => 'Inserted '.$row_counts.' rows.']));
+
+			$response->setStatusCode(Response::HTTP_OK);
+
+			return $response;
+		}
+
+		$response->setStatusCode(Response::HTTP_BAD_REQUEST);
+
+        $response->setContent(json_encode([
+			'msg' => 'Wrong request'
+		]));
+
+        return $response;
+	}
+
+	/**
+     * @Route("/api/completed", name="completed")
+     */
+    public function completed(Connection $connection, Request $request): Response
+    {
+		$request_content = strval($request->getContent());
+    	$params = json_decode($request_content, true);
+
+		$user_id = $request->query->get('user_id');
+
+		$response = new Response();
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+
+		if($params){
+			$user_id = is_null($user_id) ? (array_key_exists("user_id", $params) ? $params["user_id"] : null) : $user_id;
+		}
+
+		$completed_activities = [];
+
+		if(isset($user_id)){
+			try {
+				$resultSet = $connection->executeQuery("
+				select DISTINCT na.id from new_task_progress ntp
+				LEFT join new_task nt ON nt.id=ntp.task_id
+				LEFT join new_activity na on nt.activity_id=na.id
+				WHERE ntp.state = 'splnene'
+				AND ntp.user_id = ?", [
+					$user_id
+				]);
+
+				$users_activities = $resultSet->fetchAllAssociative();
+
+			} catch (\Throwable $th) {
+				$response->setStatusCode(Response::HTTP_BAD_REQUEST);
+
+				$response->setContent(json_encode([
+					'msg' => 'Could not be inserted into DB.',
+					'error' => $th->getMessage()
+				]));
+
+				return $response;
+			}
+
+			foreach ($users_activities as $u => $ua) {
+				$activity_id = $ua['id'];
+
+				$resultSet = $connection->executeQuery("select ntp.task_id from new_task_progress ntp
+				LEFT join new_task nt ON nt.id=ntp.task_id
+				LEFT join new_activity na on nt.activity_id=na.id
+				WHERE ntp.state = 'splnene'
+				AND ntp.user_id = ?
+				AND nt.activity_id = ?
+				", [
+					$user_id,
+					$activity_id
+				]);
+
+				$completed_tasks = $resultSet->fetchAllAssociative();
+				$completed_tasks_ids = [];
+				$all_tasks_ids = [];
+
+				foreach ($completed_tasks as $task) {
+					$completed_tasks_ids[] = $task['task_id'];
+				}
+
+				$resultSet = $connection->executeQuery("SELECT * FROM new_task WHERE activity_id = ?", [
+					$activity_id
+				]);
+
+				$all_tasks = $resultSet->fetchAllAssociative();
+
+				foreach ($all_tasks as $task) {
+					$all_tasks_ids[] = $task['id'];
+				}
+
+				$result = array_diff($all_tasks_ids, $completed_tasks_ids);
+
+				/*print_r(json_encode($completed_tasks_ids));
+				echo "<br>";
+				print_r(json_encode($all_tasks_ids));
+				echo "<br>";
+				echo count($result);
+				echo "<br>";
+				echo "<br>";*/
+
+				if(count($result) == 0){
+					$resultSet = $connection->executeQuery('SELECT na.id, na.name, na.img_url, na.level, na.activity_type, nac.id as age_category_id, nac.name as age_category
+					FROM new_activity na LEFT JOIN new_age_category nac ON na.age_category_id=nac.id
+					WHERE 1=1 AND na.id = ?', [
+							$activity_id
+					]);
+
+					$aktivita = $resultSet->fetchAllAssociative()[0];
+
+					$resultSet = $connection->executeQuery('SELECT * FROM new_task WHERE activity_id = ?', [
+						$activity_id
+					]);
+
+					$aktivita['ulohy'] = $resultSet->fetchAllAssociative();
+
+					$completed_activities[] = $aktivita;
+				}
+			}
+
+			//die();
+
+			$response->setContent(json_encode($completed_activities));
 
 			$response->setStatusCode(Response::HTTP_OK);
 
